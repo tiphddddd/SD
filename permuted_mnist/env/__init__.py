9a86129c10a5ef140ec0b9928ea99e9829c4b9e7
brisiq <158595985+brisiq@users.noreply.github.com>
2025-11-11 16:36:25 +0100

    Delete permuted_mnist directory

--- permuted_mnist/env/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PermutedMNIST environment module
-"""
-from .permuted_mnist import PermutedMNISTEnv
-
-# Export the environment as the default
-Env = PermutedMNISTEnv
-
-__all__ = ['PermutedMNISTEnv', 'Env']
